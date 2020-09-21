4434d016c2383c983758ece8d4e8f1780315a534
kih1024 <rladlsgh654@naver.com>
2020-09-21 21:57:36 +0900

    Modify video controller,router

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,19 +1,24 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>{
-    // query 안에 있는 term을 가져오고 싶을때, 이건 req.query.term 을 한것과 같음. es6에 나온 새로운 문법
-    // 그리고 이것은 
-    // const searchingBy = req.query.term 
-    // 을 한거와 같다.
-    const {query: { term : searchingBy }} = req
-    res.render("search", { pageTitle: "Search", searchingBy });
-}
-export const videos = (req, res) =>
-  res.render("videos", { pageTitle: "Videos" });
+import { videos } from "../db";
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
+
+
+// query 안에 있는 term을 가져오고 싶을때, 이건 req.query.term 을 한것과 같음. es6에 나온 새로운 문법
+// 그리고 이것은 
+// const searchingBy = req.query.term 
+// 을 한거와 같다.
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
 export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "VideoDetail" });
+  res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) =>
-  res.render("editVideo", { pageTitle: "EditVideo" });
+  res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) =>
-  res.render("deleteVideo", { pageTitle: "DeleteVideo" });
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
