c008349282e4325c445b46f3c30c67b2c2b13175
kih1024 <rladlsgh654@naver.com>
2020-11-05 02:53:13 +0900

    Add multer avatar

--- middlewares.js
+++ middlewares.js
@@ -1,13 +1,14 @@
 import multer from "multer";
 import routes from "./routes";
 
-// multer : 영상파일에 넣으면 영상 파일의 url을 반환하려고 쓴거다.
+// multer : 영상파일을 넣으면 영상 파일의 url을 반환하려고 쓴거다.
 const multerVideo = multer({ dest: "uploads/videos/" });
+const multerAvatar = multer({ dest: "uploads/avatars/" });
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
-  res.locals.user = req.user || null;
+  res.locals.loggedUser = req.user || null;
   console.log(req.user);
   next(); // 다음 함수로 넘어간다는 뜻
 };
@@ -28,5 +29,6 @@ export const onlyPrivate = (req, res, next) => {
   }
 };
 
-//  이 single 함수는 req.file에 파일 정보를 저장한다.
+//  이 single 함수는 파일을 해당 dest에 저장하고 req.file에 파일 정보를 저장한다. 그래서 컨트롤러에서 file.path를 통해서 영상 url을 가져 올수 있다.
 export const uploadVideo = multerVideo.single("videoFile");
+export const uploadAvatar = multerAvatar.single("avatar");
